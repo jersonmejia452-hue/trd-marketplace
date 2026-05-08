433771275234ce093c902320813b54d55530ca9c
jersonmejia452-hue <jersonmejia452@gmail.com>
2026-05-08 00:05:36 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,68 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const users = [
+  {
+    id: 1,
+    nombre: "Admin",
+    email: "admin@unisabana.edu.co",
+    password: "1234",
+    role: "admin"
+  },
+  {
+    id: 2,
+    nombre: "Vendedor",
+    email: "vendedor@unisabana.edu.co",
+    password: "1234",
+    role: "vendedor"
+  },
+  {
+    id: 3,
+    nombre: "Comprador",
+    email: "comprador@unisabana.edu.co",
+    password: "1234",
+    role: "comprador"
+  }
+];
+
 app.get("/", (req, res) => {
   res.send("TRD Marketplace Backend funcionando");
+});
+
+app.post("/api/auth/login", (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({
+      message: "Email y password obligatorios"
+    });
+  }
+
+  if (!email.endsWith("@unisabana.edu.co")) {
+    return res.status(403).json({
+      message: "Solo se permite correo institucional"
+    });
+  }
+
+  const user = users.find(
+    (u) => u.email === email && u.password === password
+  );
+
+  if (!user) {
+    return res.status(401).json({
+      message: "Credenciales inválidas"
+    });
+  }
+
+  return res.status(200).json({
+    message: "Login exitoso",
+    user: {
+      id: user.id,
+      nombre: user.nombre,
+      email: user.email,
+      role: user.role
+    }
+  });
 });
 
 app.listen(3000, () => {

12b02d9cb1b588f612742e04776549a2d14c602d
jersonmejia452-hue <jersonmejia452@gmail.com>
2026-05-07 23:16:55 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
+const express = require("express");
+const cors = require("cors");
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("TRD Marketplace Backend funcionando");
+});
+
+app.listen(3000, () => {
+  console.log("Servidor corriendo en puerto 3000");
+});
